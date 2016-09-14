99e41b5cef70ff17757341d259de99af1f0d667b
Yonah Forst <Yonah@Yonahs-MacBook-Pro-2.local>
2016-09-14 18:20:50 +0200

    updated messageHandler to log input/output. ignore .serverless dir

---
+++ messageHandler.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var handleIncoming = message => {
+	console.log('received message:', message)
+}
+
+var parseOutgoing = message => {
+	console.log('will send message:', message)
+	return Promise.resolve(message)
+}
