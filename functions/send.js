2875a070bb28ff578c8f195cfd2c986c5821e9c9
Bernat Jufré <info@behind.design>
2016-12-13 19:08:27 +0100

    Integrating new analytics and tests

--- functions/send.js
+++ functions/send.js
@@ -5,111 +5,99 @@ var skype = require('../services/skype');
 var messenger = require('../services/messenger')
 var twilio = require('../services/twilio')
 var messageHandler = require('../messageHandler')
-var dashbot = require('../lib/dashbot')
+var analytics = require('../lib/analytics');
 var https = require('../lib/https')
 
 var stage = process.env.SERVERLESS_STAGE || 'dev'
 var secrets = require(`../secrets.${stage}.json`)
 
 module.exports.handler = (event, context, callback) => {
-	_parseMessagesFromEvent(event)
-		.then(messages => Promise.all(messages.map(_handleOutgoingMessage)))
-		.then(_formatResponse)
-		.then(res => callback(null, res))
-		.catch(e => {
-			console.log(e)
-			callback(e)
-		})
+  _parseMessagesFromEvent(event)
+      .then(messages => Promise.all(messages.map(_handleOutgoingMessage)))
+      .then(_formatResponse)
+      .then(res => callback(null, res))
+      .catch(e => {
+        console.log(e)
+        callback(e)
+      })
 }
 
-function _handleOutgoingMessage(msg) {
-	return https.parseJson(msg)
-		.then(messageHandler.parseOutgoing)
-		.then(_sendMessage)
-		.catch(e => _handleError(e, msg))
-		.then(_logToAnalytics)
+function _handleOutgoingMessage (msg) {
+  return https.parseJson(msg)
+      .then(messageHandler.parseOutgoing)
+      .then(_sendMessage)
+      .catch(e => _handleError(e, msg))
+      .then(analytics.logToAnalytics)
 }
 
-function _handleError(error, msg) {
-	console.log('error processing message:', msg)
-	console.log(error.message, error.stack)
-	var errorParams = {
-		error: error.message
-	}
-	var message = Object.assign({}, msg, errorParams)
-	return _sendMessage(message)
+function _handleError (error, msg) {
+  console.log('error processing message:', msg)
+  console.log(error.message, error.stack)
+  var errorParams = {
+    error: error.message
+  }
+  var message = Object.assign({}, msg, errorParams)
+  return _sendMessage(message)
 }
 
 // extract message from the event. two possible event sources are SNS and HTTP.
 // We need to inspect the event contents to determine which one it's from and parse them accordingly.
-function _parseMessagesFromEvent(event) {
-	//TODO: there's gotta be a better way to determine the event source
-	if (event['Records']) {
-		return _parseSnsEvent(event)
-	} else if (event['body']) {
-		return _parseHttpEvent(event)
-	} else {
-		return Promise.reject(new Error("Can't determine event source"))
-	}
+function _parseMessagesFromEvent (event) {
+  //TODO: there's gotta be a better way to determine the event source
+  if (event['Records']) {
+    return _parseSnsEvent(event)
+  } else if (event['body']) {
+    return _parseHttpEvent(event)
+  } else {
+    return Promise.reject(new Error("Can't determine event source"))
+  }
 }
 
-function _parseSnsEvent(event) {
-	var messages = event['Records'].map(r => r['Sns']['Message'])
-	return Promise.all(messages.map(https.parseJson))
+function _parseSnsEvent (event) {
+  var messages = event['Records'].map(r => r['Sns']['Message'])
+  return Promise.all(messages.map(https.parseJson))
 }
 
-function _parseHttpEvent(event) {
-	var message = event['body']
-	return https.parseJson(message)
-		.then(res => [res])
+function _parseHttpEvent (event) {
+  var message = event['body']
+  return https.parseJson(message)
+      .then(res => [res])
 }
-
-function _logToAnalytics(msg) {
-	var shouldLogToAnalytics = secrets.dashbot && secrets.dashbot.enabled
-
-	if (!shouldLogToAnalytics) {
-		return response
-	}
-
-	return dashbot.send('outgoing', msg)
-		.then(dashbotReceipt => Object.assign({}, msg, { dashbotReceipt }))
-}
-
 
 //send a single message using the apprioriate service
-function _sendMessage(msg) {
-	var service_name = msg.service_name
+function _sendMessage (msg) {
+  var service_name = msg.service_name
 
-	if (secrets[service_name] && !secrets[service_name].enabled) {
-		throw new Error('Service disabled: ' + service_name)
-	}
+  if (secrets[service_name] && !secrets[service_name].enabled) {
+    throw new Error('Service disabled: ' + service_name)
+  }
 
- 	switch (service_name) {
-		case 'messenger':
-			return messenger.sendMessage(msg.service_user_id, msg.text)
-				.then(sendReceipt => Object.assign({}, msg, { sendReceipt }))
+  switch (service_name) {
+    case 'messenger':
+      return messenger.sendMessage(msg.service_user_id, msg.text)
+          .then(sendReceipt => Object.assign({}, msg, { sendReceipt }))
 
-		case 'twilio':
-			return twilio.sendMessage(msg.service_user_id, msg.text)
-				.then(sendReceipt => Object.assign({}, msg, { sendReceipt }))
+    case 'twilio':
+      return twilio.sendMessage(msg.service_user_id, msg.text)
+          .then(sendReceipt => Object.assign({}, msg, { sendReceipt }))
 
     case 'skype':
       return skype.sendMessage(msg.service_user_id, msg.text)
-        .then(sendReceipt => Object.assign({}, msg, { sendReceipt }))
+          .then(sendReceipt => Object.assign({}, msg, { sendReceipt }))
 
-		default:
-			throw new Error('Unknown service: ' + service_name)
-	}
+    default:
+      throw new Error('Unknown service: ' + service_name)
+  }
 }
 
-function _formatResponse(res) {
-	var response = {
-		statusCode: 200,
-		headers: {
-			"Content-Type" : "application/json",
-		},
-		body: JSON.stringify(res),
-	}
-	return response
+function _formatResponse (res) {
+  var response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(res),
+  }
+  return response
 }
 
--- functions/webhook.js
+++ functions/webhook.js
@@ -4,9 +4,9 @@ var twilio = require('../services/twilio')
 var messenger = require('../services/messenger')
 var skype = require('../services/skype');
 
+var analytics = require('../lib/analytics');
 var messageHandler = require('../messageHandler')
 var sns = require('../lib/sns')
-var dashbot = require('../lib/dashbot')
 var cyrano = require('../lib/cyrano')
 
 var stage = process.env.SERVERLESS_STAGE || 'dev'
@@ -14,75 +14,75 @@ var secrets = require(`../secrets.${stage}.json`)
 
 module.exports.handler = (event, context, callback) => {
 
-	_normalizeEvent(event)
-		.then(_processEvent)
-		.then(_handleMessages)
-		.then(_formatResponse)
-		.then(result => callback(null, result.response))
-		.catch(e => {
-			console.log(e)
-			callback(e)
-		})
+  _normalizeEvent(event)
+      .then(_processEvent)
+      .then(_handleMessages)
+      .then(_formatResponse)
+      .then(result => callback(null, result.response))
+      .catch(e => {
+        console.log(e)
+        callback(e)
+      })
 }
 
-function _normalizeEvent(event) {
-	var path = event.pathParameters || event.path
-	var service_name = path.service_name
-	var body = event.body
-	var query = event.queryStringParameters || event.query
-	var method = event.method || event.httpMethod
+function _normalizeEvent (event) {
+  var path = event.pathParameters || event.path
+  var service_name = path.service_name
+  var body = event.body
+  var query = event.queryStringParameters || event.query
+  var method = event.method || event.httpMethod
 
-	if (secrets[service_name] && !secrets[service_name].enabled) {
-		return _reject('Service disabled: ' + service_name)
-	}
+  if (secrets[service_name] && !secrets[service_name].enabled) {
+    return _reject('Service disabled: ' + service_name)
+  }
 
-	return Promise.resolve({ path, service_name, body, query, method })
+  return Promise.resolve({ path, service_name, body, query, method })
 }
 
 
 // our router
-function _processEvent(ev) {
+function _processEvent (ev) {
 
-	switch (ev.service_name) {
-		case 'messenger':
-			return messenger.processEvent(ev)
-		case 'twilio':
-			return twilio.processEvent(ev)
+  switch (ev.service_name) {
+    case 'messenger':
+      return messenger.processEvent(ev)
+    case 'twilio':
+      return twilio.processEvent(ev)
     case 'skype':
       return skype.processEvent(ev);
-		default:
-			return _reject('Unknown service: ' + ev.service_name)
-	}
+    default:
+      return _reject('Unknown service: ' + ev.service_name)
+  }
 }
 
-function _handleMessages(ev) {
+function _handleMessages (ev) {
 
-	if (!Array.isArray(ev.messages)) {
-		console.log('will not handle messages for event:', ev)
-		return ev
-	}
+  if (!Array.isArray(ev.messages)) {
+    console.log('will not handle messages for event:', ev)
+    return ev
+  }
 
-	return Promise.all(ev.messages.map(_handleMessage))
-		.then(messages => Object.assign({}, ev, { messages }))
+  return Promise.all(ev.messages.map(_handleMessage))
+      .then(messages => Object.assign({}, ev, { messages }))
 }
 
-function _formatResponse(ev) {
-	if (!ev.response) {
-		throw new Error('missing response for event:', ev)
-	}
+function _formatResponse (ev) {
+  if (!ev.response) {
+    throw new Error('missing response for event:', ev)
+  }
 
-	var content_type = ev.service_name == 'twilio' ? 'application/xml' : 'application/json'
-	var body = typeof ev.response == 'object' ? JSON.stringify(ev.response) : ev.response
+  var content_type = ev.service_name == 'twilio' ? 'application/xml' : 'application/json'
+  var body = typeof ev.response == 'object' ? JSON.stringify(ev.response) : ev.response
 
-	var response = {
-		body,
-		statusCode: 200,
-	 	headers: {
-			"Content-Type" : content_type,
-		},
-	}
+  var response = {
+    body,
+    statusCode: 200,
+    headers: {
+      "Content-Type": content_type,
+    },
+  }
 
-	return Object.assign({}, ev, { response })
+  return Object.assign({}, ev, { response })
 
 }
 
@@ -118,85 +118,74 @@ function _formatResponse(ev) {
 // 	}
 // }
 
-function _handleMessage(msg) {
-	return _processThroughMsgHandler(msg)
-		// .then(_translate)
-		.then(_publishToSns)
-		.then(_logToAnalytics)
-		.catch(error => {
-			console.log('error processing message:', error, msg)
-			return Object.assign({}, msg, { error })
-		})
+function _handleMessage (msg) {
+  return _processThroughMsgHandler(msg)
+  // .then(_translate)
+      .then(_publishToSns)
+      .then(analytics.logToAnalytics)
+      .catch(error => {
+        console.log('error processing message:', error, msg)
+        return Object.assign({}, msg, { error })
+      })
 }
 
 //parse message using our custom message handler
-function _processThroughMsgHandler(msg) {
-	return messageHandler.parseIncoming(msg)
+function _processThroughMsgHandler (msg) {
+  return messageHandler.parseIncoming(msg)
 }
 
-function _publishToSns(msg) {
-	var shouldPublishToSNS = secrets.sns && secrets.sns.enabled
+function _publishToSns (msg) {
+  var shouldPublishToSNS = secrets.sns && secrets.sns.enabled
 
-	if (!shouldPublishToSNS) {
-		return response
+  if (!shouldPublishToSNS) {
+    return response
+  }
+
+  return sns.publishReceivedMessage(msg, 'msgGateway-receivedMsg')
+      .then(snsReceipt => Object.assign({}, msg, { snsReceipt }))
+}
+
+function _translate (msg) {
+  var user_id = msg.service_name + '/' + msg.service_user_id
+  return cyrano.translateIn(user_id, msg.text)
+      .then(res => {
+        return msg
+      })
+}
+
+function _formatResponseForService (messages) {
+
+	if (messages.length == 0) {
+		return;
 	}
-
-	return sns.publishReceivedMessage(msg, 'msgGateway-receivedMsg')
-		.then(snsReceipt => Object.assign({}, msg, { snsReceipt }))
-}
-
-function _logToAnalytics(msg) {
-	console.log(msg)
-	var shouldLogToAnalytics = secrets.dashbot && secrets.dashbot.enabled
-
-	if (!shouldLogToAnalytics) {
-		return response
-	}
-
-	return dashbot.send('incoming', msg)
-		.then(dashbotReceipt => Object.assign({}, msg, { dashbotReceipt }))
-
-}
-
-function _translate(msg) {
-	var user_id = msg.service_name + '/' + msg.service_user_id
-	return cyrano.translateIn(user_id, msg.text)
-		.then(res => {
-			return msg
-		})
-}
-
-function _formatResponseForService(messages) {
-
-	if (messages.length == 0) return;
 
 	var service_name = messages[0].service_name
 
-	switch (service_name) {
-		case 'messenger':
-			return messenger.formatResponse(messages)
-		case 'twilio':
-			return twilio.formatResponse(messages)
+  switch (service_name) {
+    case 'messenger':
+      return messenger.formatResponse(messages)
+    case 'twilio':
+      return twilio.formatResponse(messages)
     case 'skype':
       return skype.formatResponse(messages);
-		default:
-			return _reject('Unknown service: ' + service_name)
-	}
+    default:
+      return _reject('Unknown service: ' + service_name)
+  }
 }
 
-function _reject(errorMsg) {
-	console.log(errorMsg)
-	return Promise.reject(new Error(errorMsg))
+function _reject (errorMsg) {
+  console.log(errorMsg)
+  return Promise.reject(new Error(errorMsg))
 }
 
 
-function _resolveAll(promises) {
-	var res = promises.map(p => {
-		return p.catch(e => {
-			console.log(e)
-			return e.toString()
-		})
-	})
-	return Promise.all(res)
+function _resolveAll (promises) {
+  var res = promises.map(p => {
+    return p.catch(e => {
+      console.log(e)
+      return e.toString()
+    })
+  })
+  return Promise.all(res)
 }
 
