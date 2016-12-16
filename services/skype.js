2bfe1aca6755e4ae42cd83f4b64f124ed0ed452a
Bernat Jufré <info@behind.design>
2016-12-16 13:21:04 +0100

    Telegram implemented

--- services/skype.js
+++ services/skype.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
-var request = require('request-promise');
-var https = require('../lib/https');
-var stage = process.env.SERVERLESS_STAGE || 'dev';
-var secrets = require(`../secrets.${stage}.json`);
+var request = require('request-promise')
+var https = require('../lib/https')
+var stage = process.env.SERVERLESS_STAGE || 'dev'
+var secrets = require(`../secrets.${stage}.json`)
 
-var utils = require('../lib/utils');
+var utils = require('../lib/utils')
 
 
-var TOKEN = {};
-var service_name = 'skype';
-var SKYPE_ID = secrets.skype.app_id;
-var SKYPE_PW = secrets.skype.app_password;
+var TOKEN = {}
+var SERVICE_NAME = 'skype'
+var SKYPE_ID = secrets.skype.app_id
+var SKYPE_PW = secrets.skype.app_password
 
 var AUTH_REQUEST = {
   url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
@@ -21,73 +21,67 @@ var AUTH_REQUEST = {
     client_id: SKYPE_ID,
     client_secret: SKYPE_PW,
     scope: 'https://graph.microsoft.com/.default',
-  }
-};
+  },
+}
 
 var CHECK_REQUEST = {
   url: _getFullyQualifiedPath('/conversations'),
   method: 'POST',
   json: true,
-};
+}
 
 function processEvent(ev) {
   return _parseMessages(ev.body)
+    .then(_filterEventType)
+    .then(_formatMessage)
     .then(messages => Object.assign({}, ev, {
       messages,
       response: {
-        status: 'ok'
-      }
-    }));
+        status: 'ok',
+      },
+    }))
 }
 
 function _parseMessages(body) {
   return https.parseJson(body)
-    .then(_formatMessages)
 }
 
-function _formatMessages(json) {
+function _filterEventType(event) {
+  return event.type === 'message' ? event : null
+}
+
+function _formatMessage(json) {
+  if (!json) {
+    return []
+  }
+
   return [{
-    service_name: service_name,
+    service_name: SERVICE_NAME,
     service_user_id: json.from.id,
     text: json.text,
     timestamp: json.timestamp,
   }]
 }
 
-function sendMessage(service_user_id, message) {
-  if (typeof message === 'string' && message.length > 320) {
-    message = utils.makeParagraphs(message, 300, '.');
-  }
-
-  if (Array.isArray(message)) {
-    return sendMessage(service_user_id, message[0])
-      .then(() => {
-        if (message.length > 1) {
-          var text = message.slice(1);
-
-          setTimeout(() => sendMessage(service_user_id, text), utils.calcuatePauseForText(text[0]));
-        }
-      })
-  }
-
-  return _sendSkypeMessage(service_user_id, message);
+function sendMessage(serviceUserId, message) {
+  return utils.sendMessageInChunks(serviceUserId, message, _sendSkypeMessage)
 }
 
-function _sendSkypeMessage(service_user_id, message) {
+function _sendSkypeMessage(serviceUserId, message) {
   var body = {
     type: 'message',
     text: message,
     timestamp: new Date().toISOString(),
-  };
+  }
 
-  return makeRequest(service_user_id, body);
+  return makeRequest(serviceUserId, body)
 }
 
 function _getFullyQualifiedPath(path) {
-  return `https://skype.botframework.com/v3${path}`;
+  return `https://skype.botframework.com/v3${path}`
 }
 
-function makeRequest(service_user_id, body) {
+function makeRequest(serviceUserId, body) {
   var options = {
     method: 'POST',
     body,
@@ -95,26 +89,26 @@ function makeRequest(service_user_id, body) {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
     },
-  };
+  }
 
   if (TOKEN.token && !utils.hasTokenExpired(TOKEN)) {
-    return _ensureConversation(service_user_id, TOKEN)
-      .then(response => _sendMessageRequest(response.id, options, TOKEN));
+    return _ensureConversation(serviceUserId, TOKEN)
+      .then(response => _sendMessageRequest(response.id, options, TOKEN))
   }
 
   return request(AUTH_REQUEST)
     .then(authResponse => {
-      var response = JSON.parse(authResponse);
+      var response = JSON.parse(authResponse)
 
-      TOKEN.token = response.access_token;
-      TOKEN.expire_date = new Date().getTime() + response.expires_in * 10;
+      TOKEN.token = response.access_token
+      TOKEN.expire_date = new Date().getTime() + response.expires_in * 10
 
-      return _ensureConversation(service_user_id, TOKEN)
-        .then(response => _sendMessageRequest(response.id, options, TOKEN));
-    });
+      return _ensureConversation(serviceUserId, TOKEN)
+        .then(response => _sendMessageRequest(response.id, options, TOKEN))
+    })
 }
 
-function _ensureConversation(service_user_id, auth) {
+function _ensureConversation(serviceUserId, auth) {
   var checking = Object.assign({}, CHECK_REQUEST, {
     body: {
       bot: {
@@ -122,40 +116,40 @@ function _ensureConversation(service_user_id, auth) {
         name: secrets.skype.bot.name,
       },
       members: [{
-        id: service_user_id
+        id: serviceUserId,
       }],
     },
     headers: {
       Authorization: `Bearer ${auth.token}`,
     },
-  });
+  })
 
   return request(checking)
     .then(res => {
-      var response = typeof res === 'string' ? JSON.parse(res) : res;
-      return Promise.resolve(response);
+      var response = typeof res === 'string' ? JSON.parse(res) : res
+      return Promise.resolve(response)
     })
     .catch(e => {
-      throw new Error(e.message);
-    });
+      throw new Error(e.message)
+    })
 }
 
-function _sendMessageRequest(conversation_id, options, auth) {
+function _sendMessageRequest(conversationId, options, auth) {
   var config = Object.assign({}, options, {
-    url: _getFullyQualifiedPath(`/conversations/${conversation_id}/activities`),
+    url: _getFullyQualifiedPath(`/conversations/${conversationId}/activities`),
     headers: {
       Authorization: `Bearer ${auth.token}`,
     },
-  });
+  })
 
   return request(config)
     .then(response => response)
     .catch(e => {
-      throw new Error(e.message);
-    });
+      throw new Error(e.message)
+    })
 }
 
 module.exports = {
   processEvent,
   sendMessage,
-};
+}
